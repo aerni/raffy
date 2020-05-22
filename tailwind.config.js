9992645cf76d6dec7a9fcf62971d72c3058ed3fd
Michael Aerni <hello@michaelaerni.ch>
2020-05-22 17:00:53 +0200

    Update Tailwind CSS and add Tailwind UI

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  },
+  theme: {},
+  variants: {},
+  plugins: [require('@tailwindcss/ui')]
+}
